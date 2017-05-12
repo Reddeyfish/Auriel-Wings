7864adb88b8d4711342fa09da95774cff0779cd1
Reddeyfish <edrichd@uci.edu>
2017-05-11 17:15:58 -0700

    main commit

---
+++ Assets/AurielWings.shader
@@ -0,0 +1,86 @@
+﻿Shader "Custom/AurielWings" {
+
+    Properties {
+		_MainTex ("Texture (RGBA)", 2D) = "white" {}
+        _WaveTex ("Wave Texture", 2D) = "white" {}
+        _Gradient ("Gradient", 2D) = "white" {}
+	}
+
+    SubShader
+    {
+        Tags { "Queue"="Transparent" "RenderType"="Transparent" "IgnoreProjector" = "True" "PreviewType"="Plane" }
+
+        //subshaders are used for compatibility. If the first subshader isn't compatible, it'll attempt to use the one below it.
+        Pass
+        {
+
+            Blend SrcAlpha One
+            ZWrite Off
+            ZTest LEqual
+            Lighting Off
+
+            CGPROGRAM
+                //begin CG block
+
+                #pragma vertex vert
+                //we will use a vertex function, named "vert".
+
+                #pragma fragment frag
+                //we will use a fragment function, named "frag"
+            
+                #include "UnityCG.cginc"
+                //use a CGInclude file defining several useful functions, including our vertex function
+            
+                //declare our external properties
+                uniform sampler2D _MainTex;
+
+                uniform sampler2D _WaveTex;
+                uniform float4 _WaveTex_ST;
+
+                uniform sampler2D _Gradient;
+
+                //declare input and output structs for vertex and fragment functions
+
+                struct appdata
+                {
+                    float4 vertex : POSITION;
+                    half2 texcoord : TEXCOORD0;
+                };
+
+                struct v2f
+                {
+                    float4 pos : SV_POSITION;
+                    half2 uv : TEXCOORD0;
+                    half2 uv2 : TEXCOORD1;
+                };
+
+                v2f vert( appdata v )
+                {
+                    v2f o;
+                    o.pos = UnityObjectToClipPos (v.vertex);
+                    o.uv = v.texcoord;
+                    o.uv2 = TRANSFORM_TEX(v.texcoord, _WaveTex);
+                    o.uv2 -= float2(_Time.x, _Time.x/20);
+                    return o;
+                }
+
+                fixed4 frag (v2f i) : SV_Target
+                {
+                    // : SV_Target semantic marks the return value as the color of the fragment.
+
+                    fixed2 textureCol = tex2D(_MainTex, i.uv).ra;
+                    fixed2 waveCol = tex2D(_WaveTex, i.uv2).ra;
+
+                    fixed returnColor = max(textureCol.y * textureCol.x, waveCol.y * waveCol.x);
+
+                    fixed4 gradient = tex2D(_Gradient, fixed2(returnColor, 0.5));
+
+                    gradient.a *= textureCol.y;
+
+                    return gradient;
+                }
+            ENDCG
+        }
+    }
+    Fallback "Diffuse" //If all of our subshaders aren't compatible, use subshaders from a different shader file
+}
---
+++ Assets/WingMeshGeneration.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class WingMeshGeneration : MonoBehaviour {
+
+    [SerializeField]
+    protected float radius = 10;
+    [SerializeField]
+    protected float halfAnglePositive = 30;
+    [SerializeField]
+    protected float halfAngleNegative = -60;
+    [SerializeField]
+    protected float offset = 2;
+    [SerializeField]
+    protected float thicknessBegin = 4;
+    [SerializeField]
+    protected AnimationCurve thicknessEnd = AnimationCurve.Linear(0, 6, 1, 6);
+    [SerializeField]
+    protected float animationCycleTime = 4;
+
+    [SerializeField]
+    protected int numVertexSegments = 32;
+
+	// Use this for initialization
+	void Update () {
+
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        Mesh mesh = meshFilter.mesh;
+        mesh.Clear();
+
+        Vector3[] vertices = new Vector3[numVertexSegments * 4];
+        Vector2[] uvs = new Vector2[numVertexSegments * 4];
+        int[] triangles = new int[(numVertexSegments - 1) * 12];
+
+        //start on the right wing first, then mirror data to make the left wing
+        Vector3 center = (radius - offset) * Vector3.left;
+
+        //top edge of the right wing
+        //inner vertex first, then outer vertex
+        vertices[0] = center + (Vector3)(halfAnglePositive.DegreeToVector2() * radius);
+        vertices[1] = center + (Vector3)(halfAnglePositive.DegreeToVector2() * (radius + thicknessBegin));
+
+        uvs[0] = Vector2.zero;
+        uvs[1] = Vector2.up;
+
+        for(int i = 1; i < numVertexSegments; i++) {
+            int vertexIndexBase = 2 * i;
+            float interpolationValue = ((float)i) / (numVertexSegments - 1); //progress along the wing, range [0..1]
+            float angle = Mathf.Lerp(halfAnglePositive, halfAngleNegative, interpolationValue);
+            Vector3 direction = angle.DegreeToVector2();
+
+            vertices[vertexIndexBase] = center + (direction * radius);
+            vertices[vertexIndexBase + 1] = center + (direction * (radius + Mathf.Lerp(thicknessBegin, thicknessEnd.Evaluate((Time.time / animationCycleTime) % 1), interpolationValue)));
+
+            uvs[vertexIndexBase] = new Vector2(interpolationValue, 0);
+            uvs[vertexIndexBase + 1] = new Vector2(interpolationValue, 1);
+
+            int triangleIndexBase = 6 * (i - 1);
+            triangles[triangleIndexBase] = vertexIndexBase - 2;
+            triangles[triangleIndexBase + 1] = vertexIndexBase - 1;
+            triangles[triangleIndexBase + 2] = vertexIndexBase;
+
+            triangles[triangleIndexBase + 3] = vertexIndexBase - 1;
+            triangles[triangleIndexBase + 4] = vertexIndexBase + 1;
+            triangles[triangleIndexBase + 5] = vertexIndexBase;
+        }
+
+        int halfVertexIndex = vertices.Length / 2;
+        for(int i = 0; i < halfVertexIndex; i++) {
+            vertices[i + halfVertexIndex] = new Vector3(-vertices[i].x, vertices[i].y);
+            uvs[i + halfVertexIndex] = uvs[i];
+        }
+
+        int halfTriangleIndex = triangles.Length / 2;
+        for(int i = 0; i < halfTriangleIndex; i+= 6) {
+            triangles[halfTriangleIndex + i] = triangles[i] + halfVertexIndex;
+            triangles[halfTriangleIndex + i + 1] = triangles[i + 2] + halfVertexIndex; //swap two so that triangle normals still face the same way
+            triangles[halfTriangleIndex + i + 2] = triangles[i + 1] + halfVertexIndex;
+
+            triangles[halfTriangleIndex + i + 3] = triangles[i + 3] + halfVertexIndex;
+            triangles[halfTriangleIndex + i + 4] = triangles[i + 5] + halfVertexIndex; // again, swap order of two triangles for normal direction
+            triangles[halfTriangleIndex + i + 5] = triangles[i + 4] + halfVertexIndex;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uvs;
+        mesh.triangles = triangles;
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
+
+        //this.enabled = false;
+	}
+}
